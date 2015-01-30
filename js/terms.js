38a8d13f0007e302954771a183a1387547babe2d
Castigliego, Marco <marco.castigliego@teamaol.com>
2015-01-30 12:18:05 +0000

    adding terms

---
+++ js/terms.js
@@ -0,0 +1,36 @@
+var ref = new Firebase("https://infernonero.firebaseio.com");
+
+function authDataCallback(authData) {
+	if (authData) {
+		console.log("User " + authData.uid + " is logged in with " + authData.provider);
+		$('#social_media_wrapper').hide();
+		$('#sign').hide();
+		$('#logout').show();
+		$('#username').show();
+		$('#compare').show();
+		$('#manage').show();
+	} else {
+		console.log("User is logged out");
+		$('#logout').hide();
+		$('#username').hide();
+		$('#compare').hide();
+		$('#manage').hide();
+		$('#social_media_wrapper').show();
+		$('#sign').show();
+
+	}
+	setUserName();
+}
+
+$(document).ready(function() {
+	
+	ref.onAuth(authDataCallback);
+
+	$('#loggingout').click(function() {
+
+		ref.unauth();
+		window.location.href = "/index.html";
+
+	});
+
+}); 
