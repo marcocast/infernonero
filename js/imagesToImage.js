a4c28655b24c30dad167bc3da63246a201c60cb7
yarenty <yarenty@gmail.com>
2015-02-25 14:34:21 +0000

    added VS and bigger images

--- js/imagesToImage.js
+++ js/imagesToImage.js
@@ -1,8 +1,8 @@
 function convertImagesToSingleImage(text1, text2, src1, src2) {
 
 	if (src1 !== null && src1 !== "" && src2 !== null && src2 !== "") {
-		x = 200;
-		y = 100;
+		x = 500;
+		y = 200;
 
 		var canvas = document.createElement('CANVAS');
 
@@ -11,10 +11,13 @@ function convertImagesToSingleImage(text1, text2, src1, src2) {
 		var ctx = canvas.getContext('2d');
 		var img1 = new Image();
 		var img2 = new Image();
+		var imgVS = new Image();
 		img1.src = src1;
 		img2.src = src2;
-		ctx.drawImage(img1, 0, 0, 100, 100);
-		ctx.drawImage(img2, 100, 0, 100, 100);
+		imgVS.src = "vs.png";
+		ctx.drawImage(img1, 0, 0, 200, 200);
+		ctx.drawImage(imgVS, 200, 80, 100, 40);
+		ctx.drawImage(img2, 300, 0, 200, 200);
 
 		return canvas.toDataURL("image/png");
 
