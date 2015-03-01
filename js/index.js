a467d12c6aa73193e839e254338d2f7f38b1dd5e
Castigliego, Marco <marco.castigliego@teamaol.com>
2015-03-01 18:31:37 +0000

    adding carosel in the index page

--- js/index.js
+++ js/index.js
@@ -28,6 +28,27 @@ $(document).ready(function() {
 
 	setUserName();
 
+	$('#snapshotsCarousel').carousel({
+		interval : 5000
+	});
+
+	$('.carousel .item').each(function() {
+		var next = $(this).next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+		next.children(':first-child').clone().appendTo($(this));
+
+		for (var i = 0; i < 2; i++) {
+			next = next.next();
+			if (!next.length) {
+				next = $(this).siblings(':first');
+			}
+
+			next.children(':first-child').clone().appendTo($(this));
+		}
+	});
+
 	$('#loggingout').click(function() {
 
 		ref.unauth();
